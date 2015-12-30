b9a38ed15cb319c02fa7061276f799b30e6d7ade
Sergio Gil <sgilperez@gmail.com>
2015-12-30 01:21:43 +0100

    Bash completion

--- tlcr.cr
+++ tlcr.cr
@@ -1,12 +1,19 @@
 require "./src/tlcr"
+require "completion"
 
 client = Tlcr::Client.new(Tlcr::HTTP.new, Tlcr::Cache.new)
 
 if name = ARGV.first?
-  if page = client.get(name)
-    page.to_s(STDOUT)
+  if name =~ /^--comp/
+    completion :command do |comp|
+      comp.reply :command, client.index.available.map(&.name)
+    end
   else
-    puts "No entry for command: #{name}"
+    if page = client.get(name)
+      page.to_s(STDOUT)
+    else
+      puts "No entry for command: #{name}"
+    end
   end
 else
   puts "Please specify a command name"
