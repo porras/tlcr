798c02f8b77c0f13550e68a4e9e0ec12b78a8ad7
Sergio Gil <sgilperez@gmail.com>
2015-12-30 12:29:04 +0100

    Cache TTL

--- src/tlcr/cache.cr
+++ src/tlcr/cache.cr
@@ -1,15 +1,19 @@
 module Tlcr
-  # TODO: TTL, forced updates, progress bar
+  # TODO: forced updates, progress bar
   class Cache
-    def initialize(@directory = File.join(ENV["HOME"], ".tlcr"))
+    def initialize(@directory = File.join(ENV["HOME"], ".tlcr"), @ttl = 30.days)
     end
 
     def get(*keys)
-      unless File.exists?(filename(*keys))
+      unless hit?(*keys)
         puts "Fetching #{keys.join("::")}..."
         set(*keys, yield)
       end
       read(*keys)
+    end
+
+    private def hit?(*keys)
+      File.exists?(filename(*keys)) && Time.utc_now - File.stat(filename(*keys)).mtime < @ttl
     end
 
     private def read(*keys)
