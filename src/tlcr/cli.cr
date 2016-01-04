8b2049d5bbe9be7a62b9d35b448905d5627a7b2a
Sergio Gil <sgilperez@gmail.com>
2016-01-04 18:10:10 +0100

    Extract main program into CLI class

---
+++ src/tlcr/cli.cr
@@ -0,0 +1,40 @@
+require "option_parser"
+
+module Tlcr
+  class CLI
+    class ShowHelp < Exception
+      def self.display(*messages)
+        new(messages.map(&.to_s).join("\n"))
+      end
+    end
+
+    property! :ttl, :command
+    property? :local
+
+    def initialize
+      @ttl = 30.days
+      @local = false
+    end
+
+    def self.parse(argv)
+      new.tap do |options|
+        parser = OptionParser.new do |parser|
+          parser.banner = "Usage: #{$0} [options] [command]"
+          parser.on("-u", "--update", "Force update (default: cache for #{options.ttl.total_days} days)") { options.ttl = 0.seconds }
+          parser.on("-r", "--render", "Render local file (for authors)") { options.local = true }
+          parser.on("-h", "--help", "Show this help") { raise ShowHelp.display(parser) }
+          parser.unknown_args do |args|
+            raise ShowHelp.display(parser) if args.size != 1
+            options.command = args.first?
+          end
+        end
+
+        begin
+          parser.parse(argv)
+        rescue e : OptionParser::InvalidOption
+          raise ShowHelp.display(e.message, "", parser)
+        end
+      end
+    end
+  end
+end
